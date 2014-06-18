e524c43358d4d234d1b1f75d4f66cc21cb2d3451
Levi Cook <levicook@gmail.com>
2014-06-18 11:44:08 -0600

    connect to mongo; list live servers from root handler

--- main.go
+++ main.go
@@ -6,20 +6,30 @@ import (
 	"os"
 
 	"github.com/levicook/go-detect"
+	"labix.org/v2/mgo"
 )
 
 var (
-	port string
+	port    string
+	session *mgo.Session
 )
 
 func init() {
 	port = detect.String(os.Getenv("PORT"), "5002")
+
+	s, err := mgo.Dial(detect.String(os.Getenv("MONGOLAB_URI"), ""))
+	panicIf(err)
+	session = s
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		for _, env := range os.Environ() {
 			fmt.Fprintln(w, env)
+		}
+
+		for _, serverAddr := range session.LiveServers() {
+			fmt.Fprintln(w, serverAddr)
 		}
 	})
 
